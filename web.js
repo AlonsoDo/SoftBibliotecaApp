91664a84b95bce03190700df9f7a99b5e7cad169
AlonsoDo <correoapolosoft.hotmail.com>
2022-04-20 18:02:09 +0200

    subida6

--- web.js
+++ web.js
@@ -13,10 +13,6 @@ app.listen(process.env.PORT || 3000,function(){
     console.log('Server listening on port 3000');
 });
 
-app.get('/',function(req,res){
-    res.send('Hello World');
-});
-
 app.post('/nuevosocio',function(req,res){ 
     
     console.log(req.body.nombresocio);
